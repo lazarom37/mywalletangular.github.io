732fa3c090e487ee1e74fcca9ec7574702936914
Marcus <55485519+lazarom37@users.noreply.github.com>
2023-11-17 15:43:09 -0800

    initialize

--- src/app/component/balance/balance.component.ts
+++ src/app/component/balance/balance.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
 import { Router } from '@angular/router';
+import { EarningMoney } from '../../model/earning-money';
+import { PayingMoney } from '../../model/paying-money';
+import { UserProfile } from '../../model/user-profile';
 
 
 @Component({
@@ -14,6 +17,31 @@ export class BalanceComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  // Beginning of mock data. You would replace this with relevant Firebase code
+  // Expected result given this data: -8000
   currentBalance: number = 0;
+  mockArray = [
+    {
+      earningPaymentId: 0,
+      earningPaymentDesc: "mockEarning",
+      earningPaymentAmount: 1000
+    }
+    ,
+    {
+      payingPaymentId: 0,
+      payingPaymentDesc: "mockPaying",
+      payingPaymentAmount: 9000
+    }
+  ];
+
+  calculateBalance(): void {
+    this.currentBalance = 0;
+    this.mockArray.forEach(item => {
+      if (item.earningPaymentDesc != null) { //Verifies if item is classified as an earning. There's probably a better way of doing this
+        this.currentBalance += item.earningPaymentAmount;
+      } else if (item.payingPaymentDesc != null) { //The same but for payments
+        this.currentBalance -= item.payingPaymentAmount;
+      }
+    });
+  }
 }
